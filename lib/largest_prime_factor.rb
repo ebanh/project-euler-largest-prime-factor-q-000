da056f0760ac2588f1470abedc038ea6af594dd7
Eban Hernandez <ebanhernandez@gmail.com>
2016-02-14 05:15:53 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+
+def largest_prime_factor(input)
+  prime_factors(input).max
+end
+
+def prime_factors(input)
+  (2..(Math.sqrt(input))).select {|num| input % num == 0 && factor(num)}
+end
+
+def factor(num)
+  (2..(Math.sqrt(num))).select {|n| num % n == 0}.empty?
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(input)
+    @input = input
+  end
+
+  def number
+    prime_factors.max
+  end
+
+  def prime_factors
+    (2..(Math.sqrt(@input))).select {|num| @input % num == 0 && factor(num)}
+  end
+
+  def factor(num)
+    (2..(Math.sqrt(num))).select {|n| num % n == 0}.empty?
+  end
+end
